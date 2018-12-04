26d450fd67942bda8355a349b0fac1b837ce8b31
Wanda <doubleuevans@gmail.com>
2018-12-04 15:53:33 -0600

    added session store, me and logout middleware

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 const bcrypt = require('bcryptjs');
-const session = require('express-session')
 
 const db = require('./database/dbConfig.js');
 
 const server = express();
 //create config object
 const sessionConfig = {
-  name: 'notsession', // default is connect.sid
+  name: 'lisa', // default is connect.sid
   secret: 'asdf;lkj',
   cookie: {
     maxAge: 1000 * 60 * 10,
     secure: false, // only set it over https; in production you want this true
   },
-  httpOnly: true,
+  httpOnly: true, //no js can change/touch this code
   resave: false,
   saveUnitialized: false,
+  //added after installing connect-session-knex
+  //stores session
+  store: new KnexSessionStore({
+    tablename: 'sessions',
+    sidfieldname: 'sid', //session id field name
+    knex: db,
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+  })
 }
 
-server.use(session(sessionConfig)); //wireup config
+server.use(session(sessionConfig)); //wireup Sessionconfig management
 server.use(express.json());
 server.use(cors());
 
@@ -33,7 +43,7 @@ server.post('/api/login', (req,res) => {
     .first()
     .then(user => {
       if(user && bcrypt.compareSync(creds.password, user.password)) { //see COMPARESYNC
-        //
+        //set user id to be used in permissions
         req.session.userId = user.id;
         //passwords match and user exists by that username
         res.status(200).json({message: 'you made it!'})
@@ -68,9 +78,30 @@ server.get('/', (req, res) => {
   res.send('Its Alive!');
 });
 
-// protect this route, only authenticated users should see it
-server.get('/api/users', (req, res) => {
+function protected(req, res, next) {
+  // restricts access to only authenticated users
   if (req.session && req.session.userId) {
+    next();
+  } else {
+    //bounce them
+    res.status(401).json({ message: 'not allowed'})
+  }
+}
+
+// protect this route, only authenticated users should see it
+server.get('/api/me', protected, (req, res) => {
+    //if they are logged in, provide access to users
+    db('users')
+      .select('id', 'username', 'password') // added password to the select****
+      .where({ id: req.session.user })
+      .first()
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err));
+});
+
+server.get('/api/users', protected, (req, res) => {
     //if they are logged in, provide access to users
     db('users')
       .select('id', 'username', 'password') // added password to the select****
@@ -78,12 +109,20 @@ server.get('/api/users', (req, res) => {
         res.json(users);
       })
       .catch(err => res.send(err));
-  } else {
-    //bounce
-    res.status(401).json({ message: 'not allowed'})
-  }
 });
 
-
+server.get('/api/logout', (req, res) => {
+  if(req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.send('no logout')
+      } else {
+        res.send('goodbye')
+      }
+    })
+  } else {
+    res.end()
+  }
+})
 
 server.listen(3300, () => console.log('\nrunning on port 3300\n'));
